965e7d486b654fa4fe8887a63c843f54cc630560
davidjocic93 <davidjocic93@gmail.com>
2018-02-12 16:33:51 +0100

    fixed insecure content error

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,6 @@
 import md5 from "crypto-js/md5";
 
-export const BASE_URL = "http://gateway.marvel.com/";
+export const BASE_URL = "https://gateway.marvel.com/";
 export const PRIV_KEY = "28eff7bde526640c986639c8fc2bfd60d9658ba0";
 export const PUBLIC_KEY = "e40572c51c3a14b54c8ece81dce8e6c5";
 export const TS = new Date().getTime();
